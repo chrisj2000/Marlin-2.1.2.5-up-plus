9b3880c0b5154b4f2b28d7c4fcc8f8c354248c76
chrisj2000 <42233475+chrisj2000@users.noreply.github.com>
2025-01-26 15:13:15 +0800

    pin changes

--- Marlin-2.1.2.5/Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_common.h
+++ Marlin-2.1.2.5/Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_common.h
@@ -218,3 +218,19 @@
   #define SPI_FLASH_MISO_PIN                PB14
   #define SPI_FLASH_MOSI_PIN                PB15
 #endif
+
+#if HAS_TMC_UART
+#define X_SERIAL_TX_PIN PA13
+#define X_SERIAL_RX_PIN PA13
+
+#define Y_SERIAL_TX_PIN PA1
+#define Y_SERIAL_RX_PIN PA1
+
+#define Z_SERIAL_TX_PIN PA6
+#define Z_SERIAL_RX_PIN PA6
+
+#define E0_SERIAL_TX_PIN PA3
+#define E0_SERIAL_RX_PIN PA3
+
+#define TMC_BAUD_RATE 19200
+#endif // TMC2208 || TMC2209
